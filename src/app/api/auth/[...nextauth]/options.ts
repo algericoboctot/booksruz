c96cd3a7cf55cef2ae2d624ed12b0900f23caaf9
jongbocs <albocs@gmail.com>
2023-10-20 18:44:35 +0800

    commit changes

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,4 @@
 import type { NextAuthOptions } from "next-auth";
-import { compare } from 'bcrypt';
 import CredentialsProvider from "next-auth/providers/credentials";
 import customersLogin from "@/libs/frontend/fetchCustomers";
 import { ICustomers } from "@/interfaces/frontend/customers";
@@ -22,23 +21,17 @@ export const options: NextAuthOptions = {
                 }
             },
             async authorize(credentials: Credentials | undefined) {
-                if (credentials?.password) { // Check if password is not undefined
-                    const customers = await customersLogin();
-
-                    const matchingCustomer = customers.find((customer: ICustomers) => {
-                        return credentials?.email === customer.email;
-                    });
-
-                    if (matchingCustomer) {
-                        const passwordMatch = await compare(credentials.password, matchingCustomer.password); // Use credentials.password here
-
-                        if (passwordMatch) {
-                            return matchingCustomer;
-                        }
-                    }
+                const customers = await customersLogin();
+                
+                const matchingCustomer = customers.find((customer: ICustomers) => {
+                    return credentials?.email === customer.email && credentials?.password === customer.password;
+                });
+            
+                if (matchingCustomer) {
+                    return matchingCustomer; // Return the matching customer
+                } else {
+                    return null; // Return null if no matching customer is found
                 }
-
-                return null;
             }
         })
     ]
